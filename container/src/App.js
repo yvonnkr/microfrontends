f4e379c82eeee8924d8316801f8d25449bcbaded
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2020-12-18 15:57:18 +0000

    minor fix

--- container/src/App.js
+++ container/src/App.js
@@ -4,8 +4,6 @@ import MarketingApp from './components/MarketingApp';
 const App = () => {
   return (
     <div>
-      <h1>Container App</h1>
-      <hr />
       <MarketingApp />
     </div>
   );
