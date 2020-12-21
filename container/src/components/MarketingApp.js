7e90b62018578d16e22f3bb0639db272c6b30196
yvonnkr <34454562+yvonnkr@users.noreply.github.com>
2020-12-21 14:53:48 +0000

    Implemented multitier navigation

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -1,11 +1,24 @@
 import React, { useEffect, useRef } from 'react';
+import { useHistory } from 'react-router-dom';
 import { mount } from 'marketing/MarketingApp';
 
 const MarketingApp = () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
+    const { onParentNavigate } = mount(ref.current, {
+      onNavigate: (location) => {
+        const { pathname: currentPathname } = history.location;
+        const { pathname: nextPathname } = location;
+
+        if (currentPathname !== nextPathname) {
+          history.push(`${nextPathname}`);
+        }
+      },
+    });
+
+    history.listen(onParentNavigate);
   }, []);
 
   return <div ref={ref}></div>;
--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,9 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate(location) {
+      const { pathname: currentPathname } = history.location;
+      const { pathname: nextPathname } = location;
+
+      if (currentPathname !== nextPathname) {
+        history.push(`${nextPathname}`);
+      }
+    },
+  };
 };
 
 // if running in isolation/dev
@@ -11,7 +29,7 @@ if (process.env.NODE_ENV === 'development') {
   const devRoot = document.getElementById('_marketing-dev-root');
 
   if (devRoot) {
-    mount(devRoot);
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
   }
 }
 
